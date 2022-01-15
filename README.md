4c29de80061023170de0b60d3c4bec22b97899b1
Brayden Hill <hillbgh@aggiemail.usu.edu>
2022-01-14 23:53:50 -0700

    Operating prototype complete

---
+++ README.md
@@ -0,0 +1,3 @@
+This project validates html and css files with the w3c validator API.
+
+Simply place the script within the directory containing the files and run, an output will be printed to the console regarding basic pass / fail.
---
+++ validate.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+# Author: Brayden Hill | Hillbgh@gmail.com
+
+val_url = "https://validator.w3.org/nu/"
+
+if __name__ == '__main__':
+    local_dir = os.getcwd()
+    local_files = os.listdir(local_dir)
+    send_files = []
+
+    for name in local_files:
+        if ".html" in name or ".css" in name:
+            send_files.append(name)
+
+    for item in send_files:
+        with open(item) as file:
+            raw_html = file.read()
+            headers = {'Content-type':'text/html; charset=UTF-8', 'out':'gnu'}
+            response = requests.post(val_url, headers=headers, data=raw_html)
+
+            parse_response = response.content
+
+            if b"There were errors" in parse_response:
+                print(item + " : Failed")
+            else:
+                print(item + " : Success")
