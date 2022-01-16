cf307280d2e68f4193ebac736b54ec4a8da0a955
Brayden Hill <Hillbgh@gmail.com>
2022-01-15 17:41:28 -0700

    Added error messages if something fails

--- validate.py
+++ validate.py
@@ -2,6 +2,7 @@
 import os
 
 import requests
+from bs4 import BeautifulSoup
 
 """ This script will validate all local directory html and css files against the w3c public API.
     It then prints a basic report to the console containing the success or failure of each validated file."""
@@ -11,10 +12,11 @@ __email__: "hillbgh@gmail.com"
 
 url = "https://validator.w3.org/nu/"
 
-if __name__ == '__main__':
+def main():
     local_dir = os.getcwd()
     local_files = os.listdir(local_dir)
     send_files = []
+    errors = {}
 
     for name in local_files:
         if ".html" in name or ".css" in name:
@@ -28,11 +30,28 @@ if __name__ == '__main__':
             headers = {'Content-type': 'text/html; charset=UTF-8', 'out': 'gnu'}
             response = requests.post(url, headers=headers, data=raw_html)
 
-            parse_response = response.content
+            response = response.content
 
-            if b"There were errors" in parse_response:
-                print(item + " : Failed")
+            if b"There were errors" in response:
+                print(f"{item} : Failed")
+                errors[item] = response
             else:
-                print(item + " : Success")
+                print(f"{item} : Success")
+    print("====== End ======\n\n")
 
-    print("====== End ======")
+    if errors:
+        parse_response_html(errors)
+
+
+def parse_response_html(error_dict):
+    for key in error_dict:
+        print(f"====== Errors within {key} ======")
+        soup = BeautifulSoup(error_dict[key], "html.parser")
+        all_errors = soup.find("div", id="results").find_all("p", {"class": None})
+        for error in all_errors:
+            error = error.text
+            print(error)
+        print("\n\n")
+
+if __name__ == '__main__':
+   main()
